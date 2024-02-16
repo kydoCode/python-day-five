2928ff5bb369bbb209deefd2073d1c16feb3961d
Kody <157696416+kydoCode@users.noreply.github.com>
2024-02-16 14:00:02 +0100

    Commiting genuine functional compute program

--- exercice20.py
+++ exercice20.py
@@ -13,6 +13,17 @@ def diviser(a,b):
     else:
         return "Erreur: Division par zéro !"
 
-a = int(input"Entrez le premier nombre: ))
+a = int(input("Entrez le premier nombre: "))
 b = int(input("Entrez le deuxième nombre: "))
 operation = input("Entrez l'opération à effectuer (+, -, *, /): ")
+
+if operation == "+":
+    print(additionner(a,b))
+elif operation == "-":
+    print(soustraire(a, b))
+elif operation == "*":
+    print(multiplier(a, b))
+elif operation == "/":
+    print(diviser(a, b))
+else:
+    print("Opération inconnue.")
